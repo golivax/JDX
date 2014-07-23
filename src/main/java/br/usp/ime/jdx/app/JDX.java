golivax/JDX

commit 68bb3945bae24181d1d9b34c121d6d5eff354445

Huge change. Now there is a method entity and dependencies are defined between methods (Dependency Extractor and associated classes were largely refactored). This enables lower level analyses. Dependencies between types can be retrieved based on those method dependencies. The same goes for dependencies between compilation units. All tests passing :-). However, there is still some work left to be done: unit test should be rewritten to assert dependencies between methods (and not between types). The implicit dependencies from constructors to attribs<> still beed to be captured.

src/main/java/br/usp/ime/jdx/app/JDX.java
@@ -7,11 +7,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import br.usp.ime.jdx.entity.DependencyReport;
 import br.usp.ime.jdx.entity.SourceCodeUnit;
+import br.usp.ime.jdx.entity.dependency.DependencyReport;
 import br.usp.ime.jdx.filter.Filter;
 import br.usp.ime.jdx.filter.JavaNativeClassFilter;
-import br.usp.ime.jdx.processor.extractor.CallDependencyExtractor;
+import br.usp.ime.jdx.processor.extractor.DependencyExtractor;
 import br.usp.ime.jdx.util.filesystem.FilesystemUtils;
 
 public class JDX {
@@ -24,7 +24,7 @@ public class JDX {
 	public DependencyReport calculateDepsFrom(String sourceDir, 
 			boolean recursive, String globPattern, Filter classFilter) {
 		
-		CallDependencyExtractor extractor = new CallDependencyExtractor();
+		DependencyExtractor extractor = new DependencyExtractor();
 		
 		String[] paths = FilesystemUtils.getPathsFromSourceDir(
 				sourceDir, globPattern, recursive);
@@ -39,7 +39,7 @@ public class JDX {
 	public DependencyReport calculateDepsFrom(List<String> sourceDirs, 
 			boolean recursive, String globPattern, Filter classFilter) {
 
-		CallDependencyExtractor extractor = new CallDependencyExtractor();
+		DependencyExtractor extractor = new DependencyExtractor();
 
 		String[] paths = FilesystemUtils.getPathsFromSourceDirs(
 				sourceDirs, globPattern, recursive);
@@ -74,7 +74,8 @@ public class JDX {
 		String fileSeparator = FileSystems.getDefault().getSeparator();
 		System.out.println(fileSeparator);
 		
-		String rootDir = "C:\\tmp\\ant\\src\\main";
+		String rootDir = "C:/tmp/maven/maven/maven-3/trunk/maven-core/src/"
+				+ "main/java/org/apache";
 				
 		JDX jdx = new JDX();
 		

commit 77f738eb38ddc55e795186e405875b36ec9ce444

Implemented the detection of strict type dependencies, i.e., inheritance and interface implementation. Refactored a lot of stuff. For now, it is not possible to distinguish between inheritance and interface implementations. Tests still need to be rewritten to assert method call dependencies. Found a bug I introduced back in commit 68bb394. Will fix it in the next commit

src/main/java/br/usp/ime/jdx/app/JDX.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 import br.usp.ime.jdx.entity.SourceCodeUnit;
 import br.usp.ime.jdx.entity.dependency.DependencyReport;
-import br.usp.ime.jdx.entity.dependency.MethodCallDependency;
 import br.usp.ime.jdx.filter.Filter;
 import br.usp.ime.jdx.filter.JavaNativeClassFilter;
 import br.usp.ime.jdx.processor.extractor.DependencyExtractor;
@@ -75,7 +74,8 @@ public class JDX {
 		String fileSeparator = FileSystems.getDefault().getSeparator();
 		System.out.println(fileSeparator);
 		
-		String rootDir = "C:/Users/user/workspace/tricky1/src/tricky3";
+		String rootDir = "C:/tmp/maven/maven/maven-3/trunk/maven-core/src/"
+				+ "main/java/org/apache";
 				
 		JDX jdx = new JDX();
 		
@@ -83,10 +83,7 @@ public class JDX {
 				rootDir, true, "*.java", new JavaNativeClassFilter());
 		
 		System.out.println(depReport.getMethodCallDependencies().size());
-		for(MethodCallDependency dep : depReport.getMethodCallDependencies()){
-			System.out.println(dep);
-		}
-		
+		System.out.println(depReport.getMethodCallDependencies());
 		System.out.println(new Date(System.currentTimeMillis()));
 	
 	}

src/main/java/br/usp/ime/jdx/entity/Type.java
@@ -76,7 +76,7 @@ public class Type extends NamedEntity implements Serializable{
 
 	@Override
 	public String toString(){
-		return getName();
+		return getFQN();
 	}
 	
 	public Set<Method> getConstructors(){

src/main/java/br/usp/ime/jdx/entity/dependency/DependencyReport.java
@@ -13,31 +13,59 @@ public class DependencyReport implements Serializable{
 
 	private static final long serialVersionUID = 2294342917700842739L;
 
-	private MultiKeyMap<Method,MethodCallDependency> dependenciesMap = 
+	private MultiKeyMap<Type,TypeDependency> typeDependenciesMap = 
+			new MultiKeyMap<>();
+	
+	private MultiKeyMap<Method,MethodCallDependency> callDependenciesMap = 
 			new MultiKeyMap<>();
 	
 	public DependencyReport(){
 		
 	}
 	
-	public void addDependency(Method client, Method supplier){
-		if (!dependenciesMap.containsKey(client, supplier)){
-			MethodCallDependency dependency = new MethodCallDependency(client, supplier);
-			dependenciesMap.put(client, supplier, dependency);
+	public void addDependency(Type client, Type supplier){
+		if (!typeDependenciesMap.containsKey(client, supplier)){
+			TypeDependency dependency = new TypeDependency(client, supplier);
+			typeDependenciesMap.put(client, supplier, dependency);
 		}
 		else{
-			MethodCallDependency dependency = dependenciesMap.get(client, supplier);
+			System.out.println("Warning: Trying to define two different "
+					+ "relationships between types");
+		}
+	}
+	
+	public void addDependency(Method client, Method supplier){
+		if (!callDependenciesMap.containsKey(client, supplier)){
+			MethodCallDependency dependency = new MethodCallDependency(client, supplier);
+			callDependenciesMap.put(client, supplier, dependency);
+		}
+		else{
+			MethodCallDependency dependency = callDependenciesMap.get(client, supplier);
 			dependency.increaseStrength();
 		}
 	}
 	
 	public Collection<MethodCallDependency> getMethodCallDependencies(){
-		return dependenciesMap.values();
+		return callDependenciesMap.values();
 	}
 	
-	public Collection<Dependency<Type>> getTypeDependencies(){
+	public Collection<TypeDependency> getTypeDependencies(
+			boolean inferFromMethodCalls){
 		
-		MultiKeyMap<Type,Dependency<Type>> typeDependenciesMap = 
+		if(inferFromMethodCalls){
+			Collection<TypeDependency> typeDependencies = 
+					inferDependenciesFromMethodCalls();
+			
+			typeDependencies.addAll(this.typeDependenciesMap.values());
+			return typeDependencies;
+		}
+		else{
+			return typeDependenciesMap.values();
+		}
+	}
+	
+	private Collection<TypeDependency> inferDependenciesFromMethodCalls() {
+		MultiKeyMap<Type,TypeDependency> typeDependenciesMap = 
 				new MultiKeyMap<>();
 		
 		Collection<MethodCallDependency> methodCallDependencies = 
@@ -55,13 +83,13 @@ public class DependencyReport implements Serializable{
 			if(!clientType.equals(supplierType)){
 			
 				if (typeDependenciesMap.containsKey(clientType,supplierType)){
-					Dependency<Type> dep = 
+					TypeDependency dep = 
 							typeDependenciesMap.get(clientType,supplierType);
 					
 					dep.increaseStrength(methodCallDependency.getStrength());
 				}
 				else{
-					Dependency<Type> typeDep = new Dependency<Type>(clientType, 
+					TypeDependency typeDep = new TypeDependency(clientType, 
 							supplierType, methodCallDependency.getStrength());
 					
 					typeDependenciesMap.put(clientType, supplierType, typeDep);
@@ -71,13 +99,17 @@ public class DependencyReport implements Serializable{
 		
 		return typeDependenciesMap.values();
 	}
-	
-	public Collection<Dependency<CompUnit>> getCompUnitDependencies(){
+
+	public Collection<Dependency<CompUnit>> getCompUnitDependencies(
+			boolean inferFromMethodCalls){
+		
 		MultiKeyMap<CompUnit,Dependency<CompUnit>> cuDependenciesMap = 
 				new MultiKeyMap<CompUnit,Dependency<CompUnit>>();
 		
-		Collection<Dependency<Type>> typeDependencies = getTypeDependencies();
-		for(Dependency<Type> typeDependency : typeDependencies){
+		Collection<TypeDependency> typeDependencies = getTypeDependencies(
+				inferFromMethodCalls);
+		
+		for(TypeDependency typeDependency : typeDependencies){
 			
 			CompUnit clientCU = typeDependency.getClient().getCompUnit();
 			CompUnit supplierCU = typeDependency.getSupplier().getCompUnit();
@@ -101,10 +133,13 @@ public class DependencyReport implements Serializable{
 	}
 	
 	
-	public Dependency<Type> getTypeDependency(String clientName, String supplierName){
+	public Dependency<Type> getTypeDependency(
+			String clientName, String supplierName){
 		
-			Collection<Dependency<Type>> typeDependencies = getTypeDependencies();
-			for(Dependency<Type> typeDependency : typeDependencies){
+			Collection<TypeDependency> typeDependencies = 
+					getTypeDependencies(true);
+			
+			for(TypeDependency typeDependency : typeDependencies){
 				
 				if(typeDependency.getClient().getFQN().equals(clientName) && 
 					typeDependency.getSupplier().getFQN().equals(supplierName)){
@@ -119,7 +154,7 @@ public class DependencyReport implements Serializable{
 
 	public String toString(){
 		StringBuilder builder = new StringBuilder();
-		for (Dependency<Method> dependency : dependenciesMap.values()){
+		for (Dependency<Method> dependency : callDependenciesMap.values()){
 			
 			builder.append(dependency.toString());
 			builder.append("\n");

src/main/java/br/usp/ime/jdx/entity/dependency/MethodCallDependency.java
@@ -14,5 +14,4 @@ public class MethodCallDependency extends Dependency<Method>{
 		super(client, supplier, strength);
 	}
 
-
 }

src/main/java/br/usp/ime/jdx/entity/dependency/TypeDependency.java
@@ -1,0 +1,17 @@
+package br.usp.ime.jdx.entity.dependency;
+
+import br.usp.ime.jdx.entity.Type;
+
+public class TypeDependency extends Dependency<Type>{
+
+	private static final long serialVersionUID = -3038818691650424132L;
+
+	public TypeDependency(Type client, Type supplier) {
+		super(client, supplier);
+	}
+
+	public TypeDependency(Type client, Type supplier, Integer strength) {
+		super(client, supplier, strength);
+	}
+
+}

src/main/java/br/usp/ime/jdx/processor/extractor/Cacher.java
@@ -155,6 +155,7 @@ public class Cacher extends FileASTRequestor{
 		String typeName = typeDeclaration.getName().toString();
 
 		ASTNode node = typeDeclaration.getParent();
+
 		while(node != null){
 			if(node instanceof TypeDeclaration){
 				TypeDeclaration superType = (TypeDeclaration)node;
@@ -208,8 +209,8 @@ public class Cacher extends FileASTRequestor{
 		return typeDeclarationMap.keySet();
 	}
 
-	public Type getType(String typeName){
-		return typeNameMap.get(typeName);
+	public Type getType(String typeQualifiedName){
+		return typeNameMap.get(typeQualifiedName);
 	}
 
 	public Type getType(TypeDeclaration typeDeclaration) {

src/main/java/br/usp/ime/jdx/processor/extractor/DependencyExtractor.java
@@ -6,6 +6,7 @@ import br.usp.ime.jdx.entity.dependency.DependencyReport;
 import br.usp.ime.jdx.filter.Filter;
 import br.usp.ime.jdx.processor.BatchCompilationUnitProcessor;
 import br.usp.ime.jdx.processor.extractor.call.CallDependencyExtractor;
+import br.usp.ime.jdx.processor.extractor.call.TypeDependencyExtractor;
 
 public class DependencyExtractor{
 
@@ -23,10 +24,17 @@ public class DependencyExtractor{
 
 		batchCompilationUnitProcessor.run(sourceDirs, cacher, paths);
 
+		DependencyReport depReport = new DependencyReport();
+		
 		CallDependencyExtractor callDepExtractor = 
-				new CallDependencyExtractor(cacher);
+				new CallDependencyExtractor(cacher); 
+				
+		callDepExtractor.run(depReport, classFilter);
 
-		DependencyReport depReport = callDepExtractor.run(classFilter);
+		TypeDependencyExtractor typeDepExtractor = 
+				new TypeDependencyExtractor(cacher);
+		
+		typeDepExtractor.run(depReport, classFilter);
 		
 		return depReport;
 	}

src/main/java/br/usp/ime/jdx/processor/extractor/call/CallDependencyExtractor.java
@@ -3,7 +3,6 @@ package br.usp.ime.jdx.processor.extractor.call;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.AssertStatement;
 import org.eclipse.jdt.core.dom.Assignment;
@@ -53,19 +52,14 @@ public class CallDependencyExtractor {
 		this.cacher = cacher;
 	}
 	
-	public DependencyReport run(Filter classFilter) {
+	public DependencyReport run(DependencyReport dependencyReport, 
+			Filter classFilter) {
 		
 		this.classFilter = classFilter;
-		
-		//Creates a new dependency report and extracts the dependencies
-		this.dependencyReport = new DependencyReport();
+		this.dependencyReport = dependencyReport;
 		
 		extractImplicitDependencies();
-		
-		//Now we process each the fields and methods of each type
-		for(TypeDeclaration typeDeclaration : cacher.getTypeDeclarations()){
-			processFieldsAndMethods(typeDeclaration);
-		}		
+		extractExplicitDependencies();	
 
 		return dependencyReport;
 	}
@@ -86,6 +80,14 @@ public class CallDependencyExtractor {
 		//default constructor from A
 	}
 	
+	private void extractExplicitDependencies() {
+		//Now we process the fields and methods of each type
+		for(TypeDeclaration typeDeclaration : cacher.getTypeDeclarations()){
+			processFieldsAndMethods(typeDeclaration);
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
 	private void processFieldsAndMethods(TypeDeclaration typeDeclaration){
 
 		//Processing fields
@@ -112,6 +114,7 @@ public class CallDependencyExtractor {
 		}
 	}
 
+	@SuppressWarnings("unchecked")
 	private void processBlock(Block codeBlock){
 		try{
 			List<Statement> statements = codeBlock.statements();
@@ -231,8 +234,8 @@ public class CallDependencyExtractor {
 	private void processTypeBinding(ITypeBinding binding){
 		if(binding != null){		
 				
-			String providerTypeName = 
-					getProviderTypeName(binding.getDeclaringClass());
+			System.out.println("What is this?!");
+			//String providerTypeName = ExtractorUtils.getQualifiedTypeName(
 				
 			//setUse(providerTypeName);
 		}
@@ -498,48 +501,32 @@ public class CallDependencyExtractor {
 			if (!binding.getDeclaringClass().isAnonymous() && 
 				!binding.getDeclaringClass().isLocal()){		
 				
-				String methodName = binding.getName();
-				List<String> parameterTypes = new ArrayList<>();
+				String providerTypeName = ExtractorUtils.getQualifiedTypeName(
+ 						binding.getDeclaringClass());
 				
-				for(ITypeBinding typeBinding : binding.getParameterTypes()){
-					parameterTypes.add(typeBinding.getName());
-				}
-				
- 				String providerTypeName = 
-						getProviderTypeName(binding.getDeclaringClass());
-				
-				Type providerType = 
-						cacher.getType(providerTypeName);
-				
-				if(providerType == null){
-					System.out.println("WARNING: Could not find binding for " + 
-						providerTypeName + " in class " + 
-						clientMethod.getContainingType().getCompUnit());
-				}
-				else{
-					Method providerMethod = 
-							providerType.getMethod(methodName, parameterTypes);
+				if (!classFilter.matches(providerTypeName)){
 					
+					String methodName = binding.getName();
+					List<String> parameterTypes = new ArrayList<>();
+					
+					for(ITypeBinding typeBinding : binding.getParameterTypes()){
+						parameterTypes.add(typeBinding.getName());
+					}
+										
+					Type providerType =	cacher.getType(providerTypeName);
+					
+					Method providerMethod =	
+							providerType.getMethod(methodName, parameterTypes);
+
 					setUse(providerMethod);
 				}
 			}
 		}
 	}
 
-	private String getProviderTypeName(ITypeBinding typeBinding) {
-		
-		String qualifiedName = typeBinding.getQualifiedName();
-		//Removing generic's type parameter
-		String providerTypeName = StringUtils.substringBefore(qualifiedName, "<");
-		return providerTypeName;
+	private void setUse(Method providerMethod){	
+		dependencyReport.addDependency(clientMethod, providerMethod);
 	}
-
-	private void setUse(Method providerMethod){
-
-		Type providerType = providerMethod.getContainingType();
-		if (!classFilter.matches(providerType.getName())){			
-				dependencyReport.addDependency(clientMethod, providerMethod);
-		}
 		
-	}
+	
 }

src/main/java/br/usp/ime/jdx/processor/extractor/call/ExtractorUtils.java
@@ -1,0 +1,16 @@
+package br.usp.ime.jdx.processor.extractor.call;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+
+public class ExtractorUtils {
+
+	public static String getQualifiedTypeName(ITypeBinding typeBinding) {
+		
+		String qualifiedName = typeBinding.getQualifiedName();
+		//Removing generic's type parameter
+		String providerTypeName = StringUtils.substringBefore(qualifiedName, "<");
+		return providerTypeName;
+	}
+	
+}

src/main/java/br/usp/ime/jdx/processor/extractor/call/TypeDependencyExtractor.java
@@ -1,0 +1,102 @@
+package br.usp.ime.jdx.processor.extractor.call;
+
+import java.util.List;
+
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+
+import br.usp.ime.jdx.entity.Type;
+import br.usp.ime.jdx.entity.dependency.DependencyReport;
+import br.usp.ime.jdx.filter.Filter;
+import br.usp.ime.jdx.processor.extractor.Cacher;
+
+/**
+ * Extracts relationships between types. For now it detects
+ * inheritance and interface implementation
+ * @author Gustavo Ansaldi Oliva {@link goliva@ime.usp.br}
+ *
+ */
+public class TypeDependencyExtractor {
+
+	private Cacher cacher;
+	private Filter classFilter;
+	private DependencyReport dependencyReport;
+	
+	public TypeDependencyExtractor(Cacher cacher){
+		this.cacher = cacher;
+	}
+	
+	public DependencyReport run(DependencyReport dependencyReport, 
+			Filter classFilter) {
+		
+		this.classFilter = classFilter;
+		this.dependencyReport = dependencyReport;
+		
+		detectClassInheritance();
+		detectInterfaceImplementationAndExtension();
+	
+		return dependencyReport;
+	}
+	
+	private void detectClassInheritance() {
+		for(TypeDeclaration typeDeclaration : cacher.getTypeDeclarations()){
+						
+			if(typeDeclaration.getSuperclassType() != null){
+				
+				Type type = cacher.getType(typeDeclaration);
+				
+				if(typeDeclaration.getSuperclassType().resolveBinding() == null){
+					
+					System.out.println("WARNING: Could not find binding for " + 
+							typeDeclaration.getSuperclassType() + " in class " + 
+							type.getCompUnit());					
+				}
+				else{
+					String parentTypeName = ExtractorUtils.getQualifiedTypeName(
+						typeDeclaration.getSuperclassType().resolveBinding());
+					
+					addDependency(type, parentTypeName);
+				}				
+			}
+		}
+	}
+
+	private void detectInterfaceImplementationAndExtension() {
+		for(TypeDeclaration typeDeclaration : cacher.getTypeDeclarations()){
+			
+			//For a class declaration, these are the interfaces that this class 
+			//implements; for an interface declaration, these are the interfaces 
+			//that this interface extends.
+			List<org.eclipse.jdt.core.dom.Type> implementedInterfaces = 
+					typeDeclaration.superInterfaceTypes();
+			
+			for(org.eclipse.jdt.core.dom.Type implementedInterface : implementedInterfaces){
+				
+				Type type = cacher.getType(typeDeclaration);
+				
+				if(implementedInterface.resolveBinding() == null){
+					
+					System.out.println("WARNING: Could not find binding for " + 
+							implementedInterface + " in class " + 
+							type.getCompUnit());	
+				}
+				else{
+					String implementedInterfaceName = 
+						ExtractorUtils.getQualifiedTypeName(
+							implementedInterface.resolveBinding());
+					
+					addDependency(type, implementedInterfaceName);
+				}
+			}
+		}
+		
+	}
+
+	private void addDependency(Type clientType, String supplierTypeName){
+
+		if (!classFilter.matches(supplierTypeName)){	
+			
+			Type providerType = cacher.getType(supplierTypeName);			
+			dependencyReport.addDependency(clientType,providerType);
+		}		
+	}
+}

src/main/java/br/usp/ime/jdx/util/printer/DependencyPrinter.java
@@ -7,7 +7,7 @@ public class DependencyPrinter {
 
 	public static String printCSV(DependencyReport dependencyReport){
 		StringBuilder builder = new StringBuilder();
-		for(Dependency dependency : dependencyReport.getTypeDependencies()){
+		for(Dependency dependency : dependencyReport.getTypeDependencies(true)){
 			builder.append(
 					dependency.getClient() + ";" + 
 					dependency.getSupplier() + ";" + 

src/test/java/br/usp/ime/jdx/processor/extractor/CallDependencyExtractorTest.java
@@ -22,7 +22,7 @@ public class CallDependencyExtractorTest {
 		"/src/test/java/br/usp/ime/jdx/processor/extractor/methodinv";
 
 	//No dep
-	
+	/**
 	@Test
 	public void shouldFindNoDeps(){
 		String rootDir = this.rootDir + "/nodep";
@@ -31,8 +31,10 @@ public class CallDependencyExtractorTest {
 		DependencyReport depReport = jdx.calculateDepsFrom(rootDir, false, 
 				"*.java", new JavaNativeClassFilter());
 		
-		assertTrue(depReport.getTypeDependencies().isEmpty());		
+		System.out.println(depReport.getMethodCallDependencies());
+		assertTrue(depReport.getMethodCallDependencies().isEmpty());		
 	}
+	*/
 	
 	//Local variable tests
 	
@@ -47,10 +49,10 @@ public class CallDependencyExtractorTest {
 		System.out.println(depReport);
 		
 		//One compilation unit dependency
-		assertEquals(1, depReport.getCompUnitDependencies().size());
+		assertEquals(1, depReport.getCompUnitDependencies(true).size());
 		
 		Dependency<CompUnit> dep = 
-				depReport.getCompUnitDependencies().iterator().next();
+				depReport.getCompUnitDependencies(true).iterator().next();
 		
 		//from A
 		assertEquals(rootDir + "/A.java", dep.getClient().getName());
@@ -72,10 +74,10 @@ public class CallDependencyExtractorTest {
 		System.out.println(depReport);
 		
 		//One compilation unit dependency
-		assertEquals(1, depReport.getCompUnitDependencies().size());
+		assertEquals(1, depReport.getCompUnitDependencies(true).size());
 		
 		Dependency<CompUnit> dep = 
-				depReport.getCompUnitDependencies().iterator().next();
+				depReport.getCompUnitDependencies(true).iterator().next();
 		
 		//from A
 		assertEquals(rootDir + "/A.java", dep.getClient().getName());
@@ -97,10 +99,10 @@ public class CallDependencyExtractorTest {
 		System.out.println(depReport);
 		
 		//One compilation unit dependency
-		assertEquals(1, depReport.getCompUnitDependencies().size());
+		assertEquals(1, depReport.getCompUnitDependencies(true).size());
 		
 		Dependency<CompUnit> dep = 
-				depReport.getCompUnitDependencies().iterator().next();
+				depReport.getCompUnitDependencies(true).iterator().next();
 		
 		//from A
 		assertEquals(rootDir + "/A.java", dep.getClient().getName());
@@ -121,10 +123,10 @@ public class CallDependencyExtractorTest {
 		System.out.println(depReport);
 		
 		//One compilation unit dependency
-		assertEquals(1, depReport.getCompUnitDependencies().size());
+		assertEquals(1, depReport.getCompUnitDependencies(true).size());
 		
 		Dependency<CompUnit> dep = 
-				depReport.getCompUnitDependencies().iterator().next();
+				depReport.getCompUnitDependencies(true).iterator().next();
 		
 		//from A
 		assertEquals(rootDir + "/A.java", dep.getClient().getName());
@@ -145,10 +147,10 @@ public class CallDependencyExtractorTest {
 		System.out.println(depReport);
 		
 		//One compilation unit dependency
-		assertEquals(1, depReport.getCompUnitDependencies().size());
+		assertEquals(1, depReport.getCompUnitDependencies(true).size());
 		
 		Dependency<CompUnit> dep = 
-				depReport.getCompUnitDependencies().iterator().next();
+				depReport.getCompUnitDependencies(true).iterator().next();
 		
 		//from A
 		assertEquals(rootDir + "/A.java", dep.getClient().getName());
@@ -170,10 +172,10 @@ public class CallDependencyExtractorTest {
 		System.out.println(depReport);
 		
 		//One compilation unit dependency
-		assertEquals(1, depReport.getCompUnitDependencies().size());
+		assertEquals(1, depReport.getCompUnitDependencies(true).size());
 		
 		Dependency<CompUnit> dep = 
-				depReport.getCompUnitDependencies().iterator().next();
+				depReport.getCompUnitDependencies(true).iterator().next();
 		
 		//from A
 		assertEquals(rootDir + "/A.java", dep.getClient().getName());
@@ -194,10 +196,10 @@ public class CallDependencyExtractorTest {
 		System.out.println(depReport);
 		
 		//One compilation unit dependency
-		assertEquals(1, depReport.getCompUnitDependencies().size());
+		assertEquals(1, depReport.getCompUnitDependencies(true).size());
 		
 		Dependency<CompUnit> dep = 
-				depReport.getCompUnitDependencies().iterator().next();
+				depReport.getCompUnitDependencies(true).iterator().next();
 		
 		//from A
 		assertEquals(rootDir + "/A.java", dep.getClient().getName());
@@ -218,10 +220,10 @@ public class CallDependencyExtractorTest {
 		System.out.println(depReport);
 		
 		//One compilation unit dependency
-		assertEquals(1, depReport.getCompUnitDependencies().size());
+		assertEquals(1, depReport.getCompUnitDependencies(true).size());
 		
 		Dependency<CompUnit> dep = 
-				depReport.getCompUnitDependencies().iterator().next();
+				depReport.getCompUnitDependencies(true).iterator().next();
 		
 		//from A
 		assertEquals(rootDir + "/A.java", dep.getClient().getName());
@@ -242,10 +244,10 @@ public class CallDependencyExtractorTest {
 		System.out.println(depReport);
 		
 		//One compilation unit dependency
-		assertEquals(1, depReport.getCompUnitDependencies().size());
+		assertEquals(1, depReport.getCompUnitDependencies(true).size());
 		
 		Dependency<CompUnit> dep = 
-				depReport.getCompUnitDependencies().iterator().next();
+				depReport.getCompUnitDependencies(true).iterator().next();
 		
 		//from A
 		assertEquals(rootDir + "/A.java", dep.getClient().getName());
@@ -266,10 +268,10 @@ public class CallDependencyExtractorTest {
 		System.out.println(depReport);
 		
 		//One compilation unit dependency
-		assertEquals(1, depReport.getCompUnitDependencies().size());
+		assertEquals(1, depReport.getCompUnitDependencies(true).size());
 		
 		Dependency<CompUnit> dep = 
-				depReport.getCompUnitDependencies().iterator().next();
+				depReport.getCompUnitDependencies(true).iterator().next();
 		
 		//from A
 		assertEquals(rootDir + "/A.java", dep.getClient().getName());
@@ -290,7 +292,7 @@ public class CallDependencyExtractorTest {
 		System.out.println(depReport);
 		
 		//One compilation unit dependency
-		assertEquals(3, depReport.getCompUnitDependencies().size());
+		assertEquals(3, depReport.getCompUnitDependencies(true).size());
 		
 		//From A to B
 		Dependency<Type> dep = 
@@ -351,7 +353,7 @@ public class CallDependencyExtractorTest {
 		System.out.println(depReport);
 		
 		//3 compilation unit dependency
-		assertEquals(3, depReport.getCompUnitDependencies().size());
+		assertEquals(3, depReport.getCompUnitDependencies(true).size());
 		
 		//From A to B
 		Dependency<Type> dep = 
@@ -411,10 +413,10 @@ public class CallDependencyExtractorTest {
 		System.out.println(depReport);
 		
 		//One compilation unit dependency
-		assertEquals(1, depReport.getCompUnitDependencies().size());
+		assertEquals(1, depReport.getCompUnitDependencies(true).size());
 		
 		Dependency<CompUnit> dep = 
-				depReport.getCompUnitDependencies().iterator().next();
+				depReport.getCompUnitDependencies(true).iterator().next();
 		
 		//Each compilation unit should store all its types
 		//In this case, the compilation unit "A.java" should have 
@@ -456,10 +458,10 @@ public class CallDependencyExtractorTest {
 		System.out.println(depReport);
 		
 		//One compilation unit dependency
-		assertEquals(1, depReport.getCompUnitDependencies().size());
+		assertEquals(1, depReport.getCompUnitDependencies(true).size());
 		
 		Dependency<CompUnit> dep = 
-				depReport.getCompUnitDependencies().iterator().next();
+				depReport.getCompUnitDependencies(true).iterator().next();
 		
 		//from A
 		assertEquals(rootDir + "/A.java", dep.getClient().getName());
@@ -481,10 +483,10 @@ public class CallDependencyExtractorTest {
 		System.out.println(depReport);
 		
 		//One compilation unit dependency
-		assertEquals(1, depReport.getCompUnitDependencies().size());
+		assertEquals(1, depReport.getCompUnitDependencies(true).size());
 		
 		Dependency<CompUnit> dep = 
-				depReport.getCompUnitDependencies().iterator().next();
+				depReport.getCompUnitDependencies(true).iterator().next();
 		
 		//from A
 		assertEquals(rootDir + "/A.java", dep.getClient().getName());
@@ -505,10 +507,10 @@ public class CallDependencyExtractorTest {
 		System.out.println(depReport);
 		
 		//One compilation unit dependency
-		assertEquals(1, depReport.getCompUnitDependencies().size());
+		assertEquals(1, depReport.getCompUnitDependencies(true).size());
 		
 		Dependency<CompUnit> dep = 
-				depReport.getCompUnitDependencies().iterator().next();
+				depReport.getCompUnitDependencies(true).iterator().next();
 		
 		//from A
 		assertEquals(rootDir + "/A.java", dep.getClient().getName());